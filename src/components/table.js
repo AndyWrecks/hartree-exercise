ef4a8c72edc942e2253cf9eedbd05d66949df667
Andis Reks <andy@commonwealthgiant.com>
2023-01-30 09:20:32 -0500

    rendering theme

--- src/components/table.js
+++ src/components/table.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 import {Table,} from "@grafana/ui";
-import {toDataFrame, FieldType, fiel} from "@grafana/data";
-
+import {FieldType, getDisplayProcessor, toDataFrame} from "@grafana/data";
 export default class TableComponent extends React.Component {
 
     state = {
-        data: buildData([
-            {
-                "people_count": { "enter": 0, "leave": 0},
-                "timestamp": "2020-05-01 00:10:56.5600010"
-            },
-            {
-                "people_count": { "enter": 2, "leave": 0},
-                "timestamp": "2020-05-01 00:12:56.5600010"
-            },
-            {
-                "people_count": { "enter": 5, "leave": 2},
-                "timestamp": "2020-05-01 01:10:56.5600010"
-            }
-        ])
+        data: toDataFrame({
+            name: 'field1',
+            fields: [
+                {name: 'test1', type:FieldType.string, values: ['test1', 'test2'], display: getDisplayProcessor()}
+            ]
+        })
     };
 
 
@@ -40,9 +30,8 @@ export default class TableComponent extends React.Component {
 
 
     render() {
-        console.log(this.state.data)
         return (
-            <Table data={this.state.data} width={100} height={100}>
+            <Table data={this.state.data} width={100} height={100} columnMinWidth={0}>
             </Table>
         )
     }
